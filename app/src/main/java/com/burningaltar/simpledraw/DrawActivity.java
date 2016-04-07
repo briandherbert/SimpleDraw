baabc61ab6550d39a54560db54d194cc555d08e7
Brian Herbert <bherbert@myyearbook.com>
2016-04-06 21:54:20 -0400

    Support SDK 8+
    Change-Id: I102c932ee3aea3a2cd40b635d339840efb2fe277

--- app/src/main/java/com/burningaltar/simpledraw/DrawActivity.java
+++ app/src/main/java/com/burningaltar/simpledraw/DrawActivity.java
@@ -1,11 +1,14 @@
 package com.burningaltar.simpledraw;
 
 import android.graphics.Color;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.meetme.android.palettebar.PaletteBar;
 
 public class DrawActivity extends AppCompatActivity implements PaletteBar.PaletteBarListener, DrawSurface.DrawListener {
     PaletteBar mPalleteBar;
@@ -69,15 +72,17 @@ public class DrawActivity extends AppCompatActivity implements PaletteBar.Palett
         super.onWindowFocusChanged(hasFocus);
         if (hasFocus) {
 
-            View decorView = getWindow().getDecorView();
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+                View decorView = getWindow().getDecorView();
 
-            decorView.setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+                decorView.setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+            }
         }
     }
 
